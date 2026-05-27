7d323fc9fbaece7d469af9bb8e4561732ee352e8
han <2873126729@qq.com>
2026-05-27 14:24:15 +0800

    修改文件名缓冲区大小

--- src/LogFile.cpp
+++ src/LogFile.cpp
@@ -81,7 +81,7 @@ void LogFile::write(const char *logline, int len) {
 std::string LogFile::getLogFileName() {
     std::string filename=basename_;
 
-    char timebuf[32];
+    char timebuf[64];
     time_t now=time(nullptr);
     struct tm tm_time;
     localtime_r(&now,&tm_time);
